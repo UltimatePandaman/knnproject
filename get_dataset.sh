8fe7801d83a6ad92539c676285b3276c24b195d7
Daniiik <48605090+Daniiik@users.noreply.github.com>
2024-03-31 14:36:02 +0200

    To english + rm zip after extraction

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# Adresa URL, ze které se má stáhnout soubor
+# URL from which the file is to be downloaded
 URL="https://datashare.ed.ac.uk/download/DS_10283_3443.zip"
 
-# Název souboru, do kterého se má uložit
+# Name of the file to be saved
 FILENAME="dataset.zip"
 
-# Adresář, do kterého se má rozbalit obsah ZIP archivu
+# Directory where the contents of the ZIP archive should be extracted
 DESTINATION="Data"
 
-# Stáhnout soubor
-echo "Stahuji dataset..."
+# Download the file
+echo "Downloading dataset..."
 wget "$URL" -O "$FILENAME"
 
-# Ověřit, zda se soubor úspěšně stáhl
+# Check if the file was downloaded successfully
 if [ $? -ne 0 ]; then
-    echo "Chyba při stahování souboru. Zkontrolujte prosím své připojení k internetu a zkuste to znovu."
+    echo "Error downloading the file. Please check your internet connection and try again."
     exit 1
 fi
 
-# Rozbalit ZIP archiv
-echo "Rozbaluji dataset..."
+# Extract the ZIP archive
+echo "Extracting dataset..."
 unzip "$FILENAME" -d "$DESTINATION"
 
-# Ověřit, zda se archiv úspěšně rozbalil
+# Check if the archive was extracted successfully
 if [ $? -ne 0 ]; then
-    echo "Chyba při rozbalování archivu. Zkontrolujte prosím, zda je soubor ve formátu ZIP a zkuste to znovu."
+    echo "Error extracting the archive. Please check if the file is in ZIP format and try again."
     exit 1
 fi
 
-echo "Dataset byl úspěšně stažen a rozbalen."
+# Delete the ZIP file after extraction
+echo "Removing the ZIP file..."
+rm -fv "$FILENAME"
+
+echo "Dataset was downloaded and extracted successfully."
