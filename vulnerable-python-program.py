655193f7004ea695a9d4b5ad36361bc62fa4c915
UltimatePandaman <ultimatepandamanqq@gmail.com>
2024-04-14 01:01:43 +0200

    removed the unnecessary files

--- vulnerable-python-program.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Enter expression: ")
-result = eval(user_input)  # Unsafe
-
-password = input("Enter expression: ")
-if password == "supersecretpassword101":
-    print("Access granted")
